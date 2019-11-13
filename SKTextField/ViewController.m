3ae6ae60ef95971fd898628b27be61585387f051
geeksk <geeksk54@gmail.com>
2019-11-13 11:20:43 +0800

    fix demo

--- SKTextField/ViewController.m
+++ SKTextField/ViewController.m
@@ -9,10 +9,10 @@
 #import "ViewController.h"
 #import "SKTextField/SKTextView.h"
 #import "SKTextField/SKTextField.h"
+#import "SKBlockTextField.h"
+
 
 @interface ViewController ()<UITextFieldDelegate>
-
-@property (nonatomic ,weak) SKTextField *textField;
 
 @end
 
@@ -21,10 +21,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    //TextField
+    //1.TextField
     SKTextField *textField = [[SKTextField alloc] init];
     [self.view addSubview:textField];
-    self.textField = textField;
     textField.frame = CGRectMake(32, 150, 300, 32);
     //如果需要自定义UITextField的delegate，请用textField.bridgeDelegate = self 代替
     textField.bridgeDelegate = self;
@@ -39,16 +38,16 @@
     textField.layer.borderColor = MAINCOLOR().CGColor;
     textField.tintColor = MAINCOLOR();
     textField.textColor = MAINCOLOR();
-    //    textField.placeholder = @"输入活动标题";
+    //    textField.placeholder = @"请输入内容";
     
     //设置占位文字颜色
     NSAttributedString *attrString = [[NSAttributedString alloc] initWithString:@"请输入内容" attributes:@{NSForegroundColorAttributeName:COLORCCCCCC(),NSFontAttributeName:textField.font}];
     textField.attributedPlaceholder = attrString;
    
-    [self.textField addTarget:self action:@selector(textFieldDidChange:) forControlEvents:UIControlEventEditingChanged];
+    [textField addTarget:self action:@selector(textFieldDidChange:) forControlEvents:UIControlEventEditingChanged];
     
     
-    //TextView
+    //2.TextView
     SKTextView *textView = [[SKTextView alloc] init];
     [self.view addSubview:textView];
     textView.frame = CGRectMake(32, 230, 300, 120);
@@ -64,6 +63,33 @@
     textView.textViewDidEndEditingBlock = ^(BOOL isEditing) {
         NSLog(@"已经结束编辑");
     };
+    
+    
+    //3.SKBlockTextField
+    SKBlockTextField *blockTextField = [[SKBlockTextField alloc] init];
+    [self.view addSubview:blockTextField];
+    blockTextField.frame = CGRectMake(32, 390, 300, 32);
+    blockTextField.limitLength = 10; //限制10个字
+    textField.placeholder = @"请输入内容";
+    blockTextField.backgroundColor = COLORF9F9F9();
+    blockTextField.returnKeyType = UIReturnKeyDone;
+    blockTextField.clearButtonMode = UITextFieldViewModeWhileEditing;
+    blockTextField.keyboardType = UIKeyboardTypeDefault; //设置键盘类型
+    blockTextField.layer.cornerRadius = 8;
+    blockTextField.layer.borderWidth = 1;
+    blockTextField.layer.borderColor = COLOREDEDED().CGColor;
+    blockTextField.tintColor = COLOR555555();
+    blockTextField.textColor = MAINCOLOR();
+    blockTextField.textFieldDidEndEditingBlock = ^(UITextField *textField) {
+        NSLog(@"textField.text结束编辑 :%@", textField.text);
+    };
+    
+    blockTextField.textFieldShouldReturnBlock = ^BOOL(UITextField *textField) {
+        [textField resignFirstResponder];
+        return YES;
+    };
+    
+    
 
 }
 
